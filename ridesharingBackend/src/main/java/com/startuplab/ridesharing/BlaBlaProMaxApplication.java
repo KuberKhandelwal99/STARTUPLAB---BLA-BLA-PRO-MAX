1a5de280e003da1d167d2b0d8944a2bde8f9632f
Dushyant Singh <k363dushyant@gmail.com>
2024-12-08 15:19:56 +0530

    ridesharingBackend --setup

---
+++ ridesharingBackend/src/main/java/com/startuplab/ridesharing/BlaBlaProMaxApplication.java
@@ -0,0 +1,13 @@
+package com.startuplab.ridesharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlaBlaProMaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlaBlaProMaxApplication.class, args);
+	}
+
+}
---
+++ ridesharingBackend/src/test/java/com/startuplab/ridesharing/BlaBlaProMaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startuplab.ridesharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlaBlaProMaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
